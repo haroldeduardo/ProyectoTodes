492f07f370deb8284083a3c51f77356bb8480dc0
julian.gilp <jagil3099@misena.edu.co>
2022-09-15 20:50:10 -0500

    Se instala SPATIE en el proyecto y se relaciona con la tabla users, ajustando el modelo a los cambios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('identificacion')->unique();
+            $table->string('nombre');
+            $table->date('fecha_nacimiento');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
