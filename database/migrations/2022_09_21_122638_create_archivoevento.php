70a039d193cdf3f53addf35fc6006bd3b7a07695
harold <hehurtado9@misena.edu.co>
2022-10-08 16:07:12 -0500

    ultimos cambios

--- database/migrations/2022_09_21_122638_create_archivoevento.php
+++ database/migrations/2022_09_21_122638_create_archivoevento.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('archivoevento', function (Blueprint $table) {
-            $table->id();
+            $table->id();   
           
             $table->string('ruta');
             $table->foreignId('id_publicacion')
