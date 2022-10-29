6d742b71cb17f6baa3ed5970545b4f56e7283b77
felipe3-A <lenovo.esucre@gmail.com>
2022-10-28 21:36:48 -0500

    bug en git 2.0

---
+++ database/seeders/Publicacionevento_seeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Publicacioneventomodels;
+
+class Publicacionevento_seeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        Publicacioneventomodels::factory(50)->create();
+    }
+}
