ce8d20cd270d62b00571bc92af68d6b7d7b2f940
camilomc17 <juan.mauna@misena.edu.co>
2022-11-08 23:35:22 -0500

    actualizacion de diferentes archivos

--- database/seeders/Publicacionevento_seeder.php
+++ database/seeders/Publicacionevento_seeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\Publicacioneventomodels;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class Publicacionevento_seeder extends Seeder
 {
@@ -19,89 +18,78 @@ class Publicacionevento_seeder extends Seeder
         Publicacioneventomodels::create([
             'nombre' => 'salud',
             'descripcion' => 'charla de salud para menores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:22:11',
+            'fecha_y_Hora'=>'2022-01-14 20:22:11',
             'lugar'=>'popayan',
             'estado'=>'activo',
             'urlExterna'=>'www',
             'responsable'=>'alonso',
-            'fecha_caducidad'=>'2022-09-20',
+            'fecha_caducidad'=>'2023-08-20',
             'tipo'=>'noticia'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'deporte',
             'descripcion' => 'charla de saludos para menores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:22:12',
+            'fecha_y_Hora'=>'2022-02-14 20:22:12',
             'lugar'=>'parque caldas',
             'estado'=>'activo',
             'urlExterna'=>'wow',
             'responsable'=>'rodrigo',
-            'fecha_caducidad'=>'2022-09-22',
+            'fecha_caducidad'=>'2023-03-22',
             'tipo'=>'noticia'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'salud',
             'descripcion' => 'charla de buenos habitos para menores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:22:12',
+            'fecha_y_Hora'=>'2022-03-14 20:20:12',
             'lugar'=>'popayan',
             'estado'=>'activo',
             'urlExterna'=>'www',
             'responsable'=>'alonso',
-            'fecha_caducidad'=>'2022-11-17',
-            'tipo'=>'noticia'
-        ]);
-        Publicacioneventomodels::create([
-            'nombre' => 'deporte',
-            'descripcion' => 'partidos relampagos para la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:22:02',
-            'lugar'=>'villa',
-            'estado'=>'activo',
-            'urlExterna'=>'wow',
-            'responsable'=>'rodrigo',
-            'fecha_caducidad'=>'2022-19-18',
+            'fecha_caducidad'=>'2023-07-17',
             'tipo'=>'noticia'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'salud',
             'descripcion' => 'vacunacion para mayores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-12 20:22:12',
+            'fecha_y_Hora'=>'2022-05-12 20:12:12',
             'lugar'=>'Hospital San Jose',
             'estado'=>'activo',
             'urlExterna'=>'www',
             'responsable'=>'Alcaldia',
-            'fecha_caducidad'=>'2021-09-20',
+            'fecha_caducidad'=>'2023-09-20',
             'tipo'=>'evento'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'baile',
             'descripcion' => 'concurso para menores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:21:12',
+            'fecha_y_Hora'=>'2022-06-14 20:21:12',
             'lugar'=>'juegos olimpicos',
             'estado'=>'activo',
             'urlExterna'=>'wow',
             'responsable'=>'Alcaldia de Popayan',
-            'fecha_caducidad'=>'2022-10-22',
+            'fecha_caducidad'=>'2023-12-22',
             'tipo'=>'evento'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'carrera atletica',
             'descripcion' => 'vacunacion para mayores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-12 20:22:12',
+            'fecha_y_Hora'=>'2022-07-12 20:22:12',
             'lugar'=>'Hospital San Jose',
             'estado'=>'activo',
             'urlExterna'=>'www',
             'responsable'=>'Alcaldia',
-            'fecha_caducidad'=>'2021-09-20',
+            'fecha_caducidad'=>'2023-02-20',
             'tipo'=>'evento'
         ]);
         Publicacioneventomodels::create([
             'nombre' => 'natacion',
             'descripcion' => 'concurso para menores de la comunidad',
-            'fecha_y_Hora'=>'2022-09-14 20:21:12',
+            'fecha_y_Hora'=>'2022-08-14 20:21:12',
             'lugar'=>'juegos olimpicos',
             'estado'=>'activo',
             'urlExterna'=>'wow',
             'responsable'=>'Alcaldia de Popayan',
-            'fecha_caducidad'=>'2022-10-22',
+            'fecha_caducidad'=>'2023-11-22',
             'tipo'=>'evento'
         ]);
     }
